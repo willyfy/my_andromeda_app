404dcaaa97bebeadf55c8931c03ac18272172c0b
FOMEKONG WILLY <fywilly@gmail.com>
2025-10-19 00:53:18 +0200

    Refactor test_helper.rb for RuboCop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,12 +12,4 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
-   # test/test_helper.rb
-
-   class ActiveSupport::TestCase
-     # ... autres configurations
-
-     # Assurez-vous d'inclure les helpers de routes pour tous les tests
-     include Rails.application.routes.url_helpers
-   end
 end
